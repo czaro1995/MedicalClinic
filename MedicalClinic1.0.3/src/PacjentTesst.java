bc9e0c0bdbe6893a89f75cb035da97cd8ccbc9d2
adrian.staszewski <adrian.staszewski@172.20.10.2>
2017-12-03 11:27:06 +0100

    03.12.2017

---
+++ MedicalClinic1.0.3/src/PacjentTesst.java
@@ -0,0 +1,10 @@
+
+public class PacjentTesst {
+
+	public static void main(String[] args) {
+		Pacjent pacjent = new Pacjent("Jan", "Kowalski", "500500500");
+		pacjent.getImie();
+		System.out.println("dupa");
+	}
+
+}
