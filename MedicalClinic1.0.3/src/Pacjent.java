a48cf2a05cc0682ef3177e28c4e13cf630bea5db
adrian.staszewski <adrian.staszewski@192.168.0.19>
2017-12-06 21:38:45 +0100

    06.12.2017

--- MedicalClinic1.0.3/src/Pacjent.java
+++ MedicalClinic1.0.3/src/Pacjent.java
@@ -1,3 +1,4 @@
+import java.util.regex.Pattern;
 
 public class Pacjent {
 	private String imie;
@@ -6,35 +7,58 @@ public class Pacjent {
 	private String login;
 	private String haslo;
 	private String numerTelefonu;
+	private Pattern patternImie = Pattern.compile("[A-Z][a-z]{2,9}+");
+	private Pattern patternNazwisko = Pattern.compile("[A-Z][a-z]{2,14}+");
+	private Pattern patternHaslo = Pattern.compile("[[A-Z]{1}[a-z]{1}[0-9]{1}]{8,}");
+	private Pattern patternNumerTelefonu = Pattern.compile("[0-9]{9,9}");
 	
-	public Pacjent(String imie, String nazwisko, String numerTelefonu) {
-		this.imie = imie;
-		this.nazwisko = nazwisko;
+	public Pacjent() {
 		this.kartaPacjenta = new KartaPacjenta();
-		this.login = imie.toLowerCase() + "." + nazwisko.toLowerCase();
-		this.haslo = "Hasl0123";
-		this.numerTelefonu = numerTelefonu;
 	}
 	
 	public String getNumerTelefonu() {
 		return numerTelefonu;
 	}
 
-	public void setNumerTelefonu(String numerTelefonu) {
-		this.numerTelefonu = numerTelefonu;
+	public boolean setNumerTelefonu(String numerTelefonu) {
+		boolean check = false;
+		if(patternNumerTelefonu.matcher(numerTelefonu).matches()) {
+			this.numerTelefonu = numerTelefonu;
+			check = true;
+		} 
+		if(!check) {
+			System.out.println("Nieprawidłowa liczba znaków lub znak niedozwolony.");
+		}
+		return check;
 	}
 
 	public String getImie() {
 		return imie;
 	}
-	public void setImie(String imie) {
-		this.imie = imie;
+	public boolean setImie(String imie) {
+		boolean check = false;
+		if(patternImie.matcher(imie).matches()) {
+			this.imie = imie;
+			check = true;
+		}
+		if(!check) {
+			System.out.println("Imie nie spełnia wymagań min 3 znaki, max 10 znakow - mozesz uzyc tylko liter");
+		}
+		return check;
 	}
 	public String getNazwisko() {
 		return nazwisko;
 	}
-	public void setNazwisko(String nazwisko) {
-		this.nazwisko = nazwisko;
+	public boolean setNazwisko(String nazwisko) {
+		boolean check = false;
+		if(patternNazwisko.matcher(nazwisko).matches()) {
+			this.nazwisko = nazwisko;
+			check = true;
+		}
+		if(!check) {
+			System.out.println("Nazwisko nie spełnia wymagań min 3 znaki, max 15 znakow - mozesz uzyc tylko liter");
+		}
+		return check;
 	}
 	public KartaPacjenta getKartaPacjenta() {
 		return kartaPacjenta;
@@ -45,14 +69,27 @@ public class Pacjent {
 	public String getLogin() {
 		return login;
 	}
-	public void setLogin(String login) {
-		this.login = login;
+	public void setLogin() {
+		if(imie != null || nazwisko != null) {
+			this.login = imie.toLowerCase() + "." + nazwisko.toLowerCase();
+		} else {
+			System.out.println("imie lub nazwisko nie jest uzupelnione");
+		}
+		
 	}
 	public String getHaslo() {
 		return haslo;
 	}
-	public void setHaslo(String haslo) {
-		this.haslo = haslo;
+	public boolean setHaslo(String haslo) {
+		boolean check = false;
+		if(patternHaslo.matcher(haslo).matches()) {
+			this.haslo = haslo;
+			check = true;
+		}
+		if(!check) {
+			System.out.println("Haslo nie spelnia wymagan - min 8 znakow, min 1 wielka litera, min 1 cyfra");
+		}
+		return check;
 	}
 	
 	public void zarezerwujWizyte(Wizyta wizyta) {
--- MedicalClinic1.0.3/src/PacjentTest.java
+++ MedicalClinic1.0.3/src/PacjentTest.java
@@ -2,8 +2,41 @@
 public class PacjentTest {
 
 	public static void main(String[] args) {
-		Pacjent pacjent = new Pacjent("Jan", "Kowalski", "500500500");
+		Pacjent pacjent = new Pacjent();
 		
+		//test wprowadzenia imienia dla obiektu pacjent
+		System.out.println("test ustawienia imienia prawidlowego - Adrian");
+		System.out.println(pacjent.setImie("Adrian"));
+		System.out.println("test ustawienia imienia z cyfra - Ad3rian");
+		System.out.println(pacjent.setImie("Ad3rian"));
+		System.out.println("test ustawienia imienia zbyt dlugiego - Adriaaaaaaaaaan");
+		System.out.println(pacjent.setImie("Adriaaaaaaaaaan"));
+		System.out.println("test ustawienia imienia zbyt krotkiego - Ad");
+		System.out.println(pacjent.setImie("Ad"));
+		
+		//test wprowadzania nazwiska dla obiektu pacjent
+		System.out.println("test ustawienia nazwiska prawidlowego - Staszewski");
+		System.out.println(pacjent.setNazwisko("Staszewski"));
+		System.out.println("test ustawienia nazwiska z cyfra - St4szewski");
+		System.out.println(pacjent.setNazwisko("St4szewski"));
+		System.out.println("test ustawienia nazwiska zbyt dlugiego - Staszeeeeeeeeeeeeeeeeewski");
+		System.out.println(pacjent.setNazwisko("Staszeeeeeeeeeeeeeeeeewski"));
+		System.out.println("test ustawienia nazwiska zbyt krotkiego - St");
+		System.out.println(pacjent.setNazwisko("St"));
+		System.out.println("test ustawienia nazwiska ze znakiem niedozwolonym - Staszewski!");
+		System.out.println(pacjent.setNazwisko("Staszewski!"));
+		
+		//test wprowadzania hasla dla obiektu pacjent
+		System.out.println("test ustawienia hasla prawidlowego - Staszewski1");
+		System.out.println(pacjent.setHaslo("Staszewski1"));
+		System.out.println("test ustawienia hasla bez cyfry - Staszewski");
+		System.out.println(pacjent.setHaslo("Staszewski"));
+		System.out.println("test ustawienia hasla bez wielkiej litery - staszewski");
+		System.out.println(pacjent.setHaslo("staszewski"));
+		System.out.println("test ustawienia hasla bez malej litery - STASZEWSKI");
+		System.out.println(pacjent.setHaslo("STASZEWSKI"));
+		System.out.println("test ustawienia hasla zbyt krotkiego - Adrian1");
+		System.out.println(pacjent.setHaslo("Adrian1"));
 	}
 
 }
